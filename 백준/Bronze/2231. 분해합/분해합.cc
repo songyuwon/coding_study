ffb3f9e78c7e4e0bc027210462adf34c7d6e0b51
songyuwon <122370093+songyuwon@users.noreply.github.com>
2024-10-01 15:27:12 +0900

    [Bronze II] Title: 분해합, Time: 288 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+int main()
+{
+	std::string str;
+	std::cin >> str;
+	int input_int = std::stoi(str);
+	int output = 0;
+	int count = 0;
+
+	for (int i = 1; i <= input_int; i++) {
+		std::vector<int> digit(str.size());
+
+		if (input_int < 10) {
+			digit[0] = i;
+		} else {
+			for (int j = 0; j < str.size(); j++) {
+				int calc = i / std::pow(10, str.size() - (j + 1));
+				digit[j] = calc % 10;
+			}
+		}
+
+		int sum = 0;
+		for (auto& a : digit) {
+			sum += a;
+		}
+
+		if (input_int == i + sum) {
+			output = i;
+			break;
+		}
+
+	}
+
+	std::cout << output;
+}
